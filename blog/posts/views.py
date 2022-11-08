25aebd16d11c970402526c450c2e9ff87b0423b0
pushparajpatil <pareshdx99@rediffmail.com>
2022-11-08 22:56:21 +0530

    decorater is added

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def decore(add):
+    def inner():
+        a=add()
+        z=a+25
+        return z
+    return inner
+
+def add():
+    return 25
+add()
