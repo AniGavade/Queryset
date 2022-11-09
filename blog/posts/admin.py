792f687b9571b4ba82508ae0a1136bfbcca39520
madhuri <madhuriadat199631@gmail.com>
2022-11-09 12:15:38 +0530

    Register models in admin.py file

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from posts.models import Auther
 # Register your models here.
+class AutherAdmin(admin.ModelAdmin):
+    list_dispaly=['name','email']
+admin.site.register(Auther,AutherAdmin)
